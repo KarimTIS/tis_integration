363783e95e01e9b0758d8794d6ba9dd5e6de5736
KarimTIS <karim@tiscontrol.com>
2025-02-12 17:49:50 +0200

    edit async_select_option 7

--- select.py
+++ select.py
@@ -116,11 +116,11 @@ class TISSecurity(SelectEntity):
         if self._attr_is_protected:
             if self._attr_read_only:
                 # revert state to the current option
-                # x = self._attr_current_option
-                # self._state = self._attr_current_option = "disarm"
+                x = self._attr_current_option
+                self._state = self._attr_current_option = "disarm"
                 self.async_write_ha_state()
-                # self._state = self._attr_current_option = x
-                # self.async_write_ha_state()
+                self._state = self._attr_current_option = x
+                self.async_write_ha_state()
                 raise ValueError("The security module is protected and read only")
             else:
                 mode = SECURITY_OPTIONS.get(option,None)
